d690c617012227bd4883650113d07bf96ecd982c
hshine1226 <hshine1226@gmail.com>
2022-01-29 21:51:52 +0900

    Add BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import CoinTracker from "./CoinTracker";
-import Movie from "./Movie";
-import ToDos from "./Todos";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./routes/Main";
+import Detail from "./routes/movie/Detail";
+import Home from "./routes/movie/Home";
 
 function App() {
   return (
-    <div>
-      {/* <ToDos />
-      <CoinTracker /> */}
-      <Movie />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/movie/:id" element={<Detail />}></Route>
+        <Route path="/movie" element={<Home />}></Route>
+        <Route path="/" element={<Main />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
