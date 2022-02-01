b8e165e0d2e3d095cfdbccaa8932ca166abce9a0
hshine1226 <hshine1226@gmail.com>
2022-02-01 21:12:22 +0900

    Add Coin detail page - using Link state that can send states other routes

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -21,7 +21,8 @@ const Coin = styled.li`
   border-radius: 15px;
   a {
     transition: color 0.2s ease-in;
-    display: block;
+    display: flex;
+    align-items: center;
     padding: 20px;
   }
   &:hover {
@@ -39,6 +40,12 @@ const Title = styled.h1`
 const Loader = styled.span`
   text-align: center;
   display: block;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
 `;
 
 interface ICoins {
@@ -75,7 +82,13 @@ function Coins() {
         <CoinsList>
           {coins.map((coin) => (
             <Coin key={coin.id}>
-              <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+              <Link to={`/${coin.id}`} state={{ name: coin.name }}>
+                <Img
+                  src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                  alt="coin"
+                />
+                {coin.name} &rarr;
+              </Link>
             </Coin>
           ))}
         </CoinsList>
