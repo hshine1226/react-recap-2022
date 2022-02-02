efec2aa5a074c96f83fd7630a86393ff2b4950a8
hshine1226 <hshine1226@gmail.com>
2022-02-02 14:17:48 +0900

    Add data type

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -25,18 +25,85 @@ const Loader = styled.span`
 interface ILocationState {
   name: string;
 }
+interface IInfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface IPriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    ath_date: string;
+    ath_price: number;
+    market_cap: number;
+    market_cap_change_24h: number;
+    percent_change_1h: number;
+    percent_change_1y: number;
+    percent_change_6h: number;
+    percent_change_7d: number;
+    percent_change_12h: number;
+    percent_change_15m: number;
+    percent_change_24h: number;
+    percent_change_30d: number;
+    percent_change_30m: number;
+    percent_from_price_ath: number;
+    price: number;
+    volume_24h: number;
+    volume_24h_change_24h: number;
+  };
+}
 
 function Coin() {
   const [loading, setLoading] = useState(true);
+  const [infoData, setInfoData] = useState<IInfoData>();
+  const [priceData, setPriceData] = useState<IPriceData>();
   const { coinId } = useParams();
   const state = useLocation().state as ILocationState;
+
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfoData(infoData);
+      setPriceData(priceData);
+    })();
+  }, []);
 
   return (
     <Container>
       <Header>
         <Title>{state?.name || "Loading..."}</Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader> : null}
+      {loading ? <Loader>Loading...</Loader> : <span></span>}
     </Container>
   );
 }
