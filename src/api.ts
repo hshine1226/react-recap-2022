2f5b0a31f6f9c6c8ced2a7ac527763f95f1bddd6
hshine1226 <hshine1226@gmail.com>
2022-02-08 21:32:41 +0900

    Add fetchCoinHistory api function

--- src/api.ts
+++ src/api.ts
@@ -17,3 +17,13 @@ export async function fetchCoinTickers(coinId: string | undefined) {
     .get(`${BASE_URL}/tickers/${coinId}`)
     .then((response) => response.data);
 }
+
+export async function fetchCoinHistory(coinId: string | undefined) {
+  const endDate = Math.floor(Date.now() / 1000);
+  const startDate = endDate - 60 * 60 * 24 * 7 * 2; // 2주전 데이터까지 조회
+  return axios
+    .get(`${BASE_URL}/coins/${coinId}/ohlcv/historical`, {
+      params: { start: startDate, end: endDate },
+    })
+    .then((response) => response.data);
+}
