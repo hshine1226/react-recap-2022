b71bb627ab7a09718b4b4d5d36dd3ad47c5563ae
hshine1226 <hshine1226@gmail.com>
2022-02-08 21:33:17 +0900

    Add fetchCoinHistory using useQuery in Chart component

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,4 +1,11 @@
-function Chart() {
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+
+function Chart({ coinId }: { coinId: string | undefined }) {
+  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
+
   return <h1>Chart!</h1>;
 }
 
